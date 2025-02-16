0540bec83440011918068e630212d6c9013363e5
marlenepaper <marlenepaper@hotmail.com>
2025-02-16 16:54:43 +0100

    compras eliminado

--- app/src/main/java/com/binaryBuddies/cinedore/adapters/FuncionesAdapter.java
+++ app/src/main/java/com/binaryBuddies/cinedore/adapters/FuncionesAdapter.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.binaryBuddies.cinedore.CompraBoletos;
 import com.binaryBuddies.cinedore.R;
 import com.binaryBuddies.cinedore.SeleccionBoletos;
 import com.binaryBuddies.cinedore.models.PeliculaModel;
