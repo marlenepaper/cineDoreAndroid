6f2d6faae73c9a1cd9c1e221d1a829dbf954a34d
marlenepaper <marlenepaper@hotmail.com>
2025-02-19 20:11:04 +0100

    agregue el calendario a crear fecha nacimiento

--- app/src/main/java/com/binaryBuddies/cinedore/CrearCuenta.java
+++ app/src/main/java/com/binaryBuddies/cinedore/CrearCuenta.java
@@ -1,5 +1,6 @@
 package com.binaryBuddies.cinedore;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -18,6 +19,8 @@ import com.binaryBuddies.cinedore.models.AuthResponse;
 import com.binaryBuddies.cinedore.models.RegisterRequest;
 import com.binaryBuddies.cinedore.network.RetrofitClient;
 import com.binaryBuddies.cinedore.services.AuthApiService;
+
+import java.util.Calendar;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -38,6 +41,28 @@ public class CrearCuenta extends AppCompatActivity {
         binding.iconoFlechaRegresar.setOnClickListener(view -> launchRegistro());
         binding.crearCuenta.setOnClickListener(view -> registerUser());
         binding.yaTienesCuenta.setOnClickListener(view -> launchIniciaSesion());
+
+
+        binding.inputFechaNacimiento.getEditText().setFocusable(false); // Bloquea la entrada manual
+        binding.inputFechaNacimiento.getEditText().setOnClickListener(view -> mostrarDatePicker());
+    }
+
+    private void mostrarDatePicker() {
+        // Obtener la fecha actual
+        final Calendar calendario = Calendar.getInstance();
+        int año = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+
+        // Crear el DatePickerDialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, year, month, dayOfMonth) -> {
+                    // Formatear la fecha a YYYY-MM-DD
+                    String fechaSeleccionada = String.format("%04d-%02d-%02d", year, month + 1, dayOfMonth);
+                    binding.inputFechaNacimiento.getEditText().setText(fechaSeleccionada);
+                }, año, mes, dia);
+
+        datePickerDialog.show();
     }
 
     public void registerUser() {
@@ -49,14 +74,14 @@ public class CrearCuenta extends AppCompatActivity {
         String identificacion = binding.inputDni.getEditText().getText().toString().trim();
         String fechaNacimiento = binding.inputFechaNacimiento.getEditText().getText().toString().trim();
 
-
-        if (nombre.isEmpty() || apellidos.isEmpty() || correoElectronico.isEmpty()  || contrasenia.isEmpty()  || telefono.isEmpty() || identificacion.isEmpty() || fechaNacimiento.isEmpty()  ) {
+        if (nombre.isEmpty() || apellidos.isEmpty() || correoElectronico.isEmpty() || contrasenia.isEmpty() ||
+                telefono.isEmpty() || identificacion.isEmpty() || fechaNacimiento.isEmpty()) {
             Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
             return;
         }
 
         AuthApiService authService = RetrofitClient.getRetrofitInstance().create(AuthApiService.class);
-        RegisterRequest request = new RegisterRequest(nombre, apellidos, correoElectronico, contrasenia,  telefono, identificacion, fechaNacimiento);
+        RegisterRequest request = new RegisterRequest(nombre, apellidos, correoElectronico, contrasenia, telefono, identificacion, fechaNacimiento);
 
         Call<AuthResponse> call = authService.register(request);
         call.enqueue(new Callback<AuthResponse>() {
@@ -66,7 +91,7 @@ public class CrearCuenta extends AppCompatActivity {
                     String token = response.body().getToken();
                     saveToken(token);
                     launchRegistro();
-                    Toast.makeText(CrearCuenta.this, "Cuenta exitosamente creada", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(CrearCuenta.this, "Cuenta creada exitosamente", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(CrearCuenta.this, "Error en el registro", Toast.LENGTH_SHORT).show();
                 }
@@ -85,7 +110,6 @@ public class CrearCuenta extends AppCompatActivity {
         editor.putString("authToken", token);
         editor.apply();
     }
-
 
     public void launchRegistro() {
         Intent intent = new Intent(CrearCuenta.this, Bienvenida.class);
